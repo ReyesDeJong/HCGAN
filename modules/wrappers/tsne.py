f1ad67c1976b5e124622b30c5eae4a113711564e
ReyesDeJong <reyes.dejong@gmail.com>
2019-04-12 20:00:39 -0400

    tsne wrapper ready

---
+++ modules/wrappers/tsne.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+PATH_TO_PROJECT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(PATH_TO_PROJECT)
+from sklearn.manifold import TSNE
+
+"""
+Standar scaler wrapper, first designed for projections
+"""
+
+
+class TSNE(object):
+
+  def __init__(self, params_to_update=None):
+    self.params = self.set_default_params()
+    if params_to_update is not None:
+      self.params.update(params_to_update)
+    self.tsne = TSNE(**self.params)
+
+  def set_default_params(self):
+    params = {
+      'n_components': 2,
+      'verbose': 0,
+      'perplexity': 40,
+      'n_iter': 300
+    }
+    return params
+
+  def fit(self, train_data_array, train_labels):
+    return train_data_array, train_labels
+
+  def transform(self, data_array):
+    return self.tsne.fit_transform(data_array)
