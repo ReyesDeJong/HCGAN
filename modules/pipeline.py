04e4997b9ebb4d4a893dc1bfa45e13d8b46d06a2
ReyesDeJong <reyes.dejong@gmail.com>
2019-04-14 19:27:51 -0400

    add printing of imension before proyection with pipeline

--- modules/pipeline.py
+++ modules/pipeline.py
@@ -17,14 +17,17 @@ class Pipeline(object):
 
   def __init__(self, list_of_pipeline_objects):
     self.list_of_pipeline_objects = list_of_pipeline_objects
+    self.pipiline_dimensions = []
 
   """
   Run first to train all objects in pipeline
   """
   def fit(self, train_data_array, train_labels):
+    self.pipiline_dimensions.append(train_data_array.shape)
     for object in self.list_of_pipeline_objects:
       train_data_array, train_labels = object.fit(
           train_data_array, train_labels)
+      self.pipiline_dimensions.append(train_data_array.shape)
     return train_data_array, train_labels
 
   """
@@ -34,4 +37,8 @@ class Pipeline(object):
   def transform(self, data_array):
     for object in self.list_of_pipeline_objects:
       data_array = object.transform(data_array)
+    print('Dimensions before projection %s' % str(self._get_dimensions_before_projection()  ))
     return data_array
+
+  def _get_dimensions_before_projection(self):
+    return [None]+self.pipiline_dimensions[-1].shape[1:]
