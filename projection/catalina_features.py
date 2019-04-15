ea95a8cc3d2e90f10eab31a4499fda451c24981d
ReyesDeJong <reyes.dejong@gmail.com>
2019-04-14 20:38:46 -0400

    see if everything is loaded in correct order

--- projection/catalina_features.py
+++ projection/catalina_features.py
@@ -16,6 +16,7 @@ from modules.pipeline import Pipeline
 from projection.projector import Projector
 from feature_extraction.clf_over_FATS_tsfresh import \
   load_and_concatenate_features
+import feature_extraction.FATS_extractor as FATS_extractor
 
 REAL_DATA_NAME = 'catalina_north9classes'
 SYN_DATA_NAME = 'catalina_amp_irregular_9classes_generated_10000'
@@ -45,27 +46,41 @@ if __name__ == '__main__':
   x_train_syn, y_train_syn, x_val_syn, y_val_syn, x_test_syn, y_test_syn = \
     syn_data_loader.get_all_sets_data(n_samples_to_get=10000)
 
-  #load features
-  path_to_real_fats_features = os.path.join(PATH_TO_PROJECT, REAL_DATA_FOLDER,
-                                            NAME_REAL_FATS_FEATURES)
-  path_to_real_tsfresh_features = os.path.join(PATH_TO_PROJECT,
-                                               REAL_DATA_FOLDER,
-                                               NAME_REAL_TSFRESH_FEATURES)
-  path_to_syn_fats_features = os.path.join(PATH_TO_PROJECT, SYN_DATA_FOLDER,
-                                           NAME_SYN_FATS_FEATURES)
-  path_to_syn_tsfresh_features = os.path.join(PATH_TO_PROJECT, SYN_DATA_FOLDER,
-                                              NAME_SYN_TSFRESH_FEATURES)
-  real_merged_features = load_and_concatenate_features(
-      [path_to_real_fats_features, path_to_real_tsfresh_features])
-  syn_merged_features = load_and_concatenate_features(
-      [path_to_syn_fats_features, path_to_syn_tsfresh_features])
+  x_train_real2, y_train_real2, x_val_real2, y_val_real2, x_test_real2, y_test_real2 = \
+    FATS_extractor.read_data_irregular_sampling(
+        path_to_real_data, magnitude_key='original_magnitude_random',
+        time_key='time_random')
+  x_train_syn2, y_train_syn2, x_val_syn2, y_val_syn2, x_test_syn2, y_test_syn2 = \
+    FATS_extractor.read_data_irregular_sampling(
+        path_to_syn_data, magnitude_key='generated_magnitude',
+        time_key='time')
+  print(np.mean(x_train_real==x_train_real2))
+  print(np.mean(y_train_real == y_train_real2))
+  print(np.mean(x_train_syn==x_train_syn2))
+  print(np.mean(y_train_syn == y_train_syn2))
+  print(np.mean(y_test_real == y_test_real2))
 
-
-  list_of_methods = [StandardScaler(), PCA(), TSNE()]
-  pipeline = Pipeline(list_of_methods)
-  projector = Projector(pipeline, show_plots=True)
-  projector.fit(x_train_real, y_train_real)
-  pipeline.print_dimensions_before_projection()
-  projector.project_and_plot_real_syn(
-      x_train_real, y_train_real, x_train_syn, y_train_syn,
-      save_fig_name='raw_curves_catalina')
+  # #load features
+  # path_to_real_fats_features = os.path.join(PATH_TO_PROJECT, REAL_DATA_FOLDER,
+  #                                           NAME_REAL_FATS_FEATURES)
+  # path_to_real_tsfresh_features = os.path.join(PATH_TO_PROJECT,
+  #                                              REAL_DATA_FOLDER,
+  #                                              NAME_REAL_TSFRESH_FEATURES)
+  # path_to_syn_fats_features = os.path.join(PATH_TO_PROJECT, SYN_DATA_FOLDER,
+  #                                          NAME_SYN_FATS_FEATURES)
+  # path_to_syn_tsfresh_features = os.path.join(PATH_TO_PROJECT, SYN_DATA_FOLDER,
+  #                                             NAME_SYN_TSFRESH_FEATURES)
+  # real_merged_features = load_and_concatenate_features(
+  #     [path_to_real_fats_features, path_to_real_tsfresh_features])
+  # syn_merged_features = load_and_concatenate_features(
+  #     [path_to_syn_fats_features, path_to_syn_tsfresh_features])
+  #
+  #
+  # list_of_methods = [StandardScaler(), PCA(), TSNE()]
+  # pipeline = Pipeline(list_of_methods)
+  # projector = Projector(pipeline, show_plots=True)
+  # projector.fit(x_train_real, y_train_real)
+  # pipeline.print_dimensions_before_projection()
+  # projector.project_and_plot_real_syn(
+  #     x_train_real, y_train_real, x_train_syn, y_train_syn,
+  #     save_fig_name='raw_curves_catalina')
